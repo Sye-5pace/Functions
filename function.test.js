585edd23fec66f98ebea2d55784f728192483367
Silas Owu <owusilasyp00@gmail.com>
2024-07-23 03:47:24 -0700

    add test cases

--- function.test.js
+++ function.test.js
@@ -1,5 +1,13 @@
-const { greet, add } = require('./script');
+const { greet, add, reverse } = require('./script');
 
 test ('it should greet user', () => {
     expect( greet('pk')).toEqual('Hello pk')
+})
+
+test ('it should add two numbers', () => {
+    expect(add(5, 5)).toBe(10)
+})
+
+test ('it reverse strings',() => {
+    expect(reverse('hello')).toBe('olleh')
 })
--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const greet = (user) => {
     return 'Hello'+ ' ' + user
 }
 
-const add = () => {
+const add = (a, b) => {
+    return a + b
+}
 
+const reverse = (str) => {
+    return str.reverse()
 }
 
 
 module.exports = {
     greet: greet,
+    reverse: reverse,
+    add: add
 }
